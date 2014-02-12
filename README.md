75fad1dbfc3337682de907f0583333381d4e0191
Andreas Paul <xorpaul@gmail.com>
2014-02-12 17:14:31 +0100

    init

--- README.md
+++ README.md
@@ -1,4 +1,108 @@
 check_linux
 ===========
 
-Linux monitoring script for Nagios/Icinga/Shinken
+Linux monitoring script for Nagios/Icinga/Shinken that wraps other check plugins and uses sar(1), /proc/meminfo, /proc/uptime and /proc/cpuinfo
+
+
+Managing many different alarm thresholds on a central monitoring server gets tedious very quickly.
+This script parses a simple YAML config file, which can contain different thresholds for every server and you can deploy it via Puppet/Chef etc.
+
+PREREQUISITES:
+--------
+* sysstat for sar(1)
+
+RECOMMENDATIONS:
+--------
+* Use the sysstat cronjob, so the script doesn't start sar in interactive mode
+* Have /usr/sbin in your path to find cron/crond
+* Move the config YAML file to a secure location where only root and the monitoring user can edit it
+
+
+USAGE:
+--------
+
+```
+$ ruby check_linux.rb --help
+Usage: check_linux [options]
+    -c, --configfile [FILE]          Config file to use in YAML format, defaults to /tmp/check_linux.cfg.yml
+    -d, --debug                      print debug information, defaults to false
+$ cat /tmp/check_linux.cfg.yml
+---
+check_sar_cpu:
+  crit: 95
+  warn: 90
+check_swap:
+  crit: 20%
+  warn: 40%
+check_load:
+  crit: "12,12,12"
+  warn: "10,10,10"
+check_sar_swap:
+  crit: 500
+  warn: 250
+check_mem:
+  crit: 5
+  warn: 10
+check_disk:
+  /:
+    crit: 5%
+    warn: 10%
+check_ntp:
+  crit: 10
+  warn: 5
+```
+
+You can edit the YAML file to overide thresholds for each check on specific servers.
+
+
+**Example**
+
+```
+$ ruby check_linux.rb 
+WARNING - load average: 9.85, 11.81, 9.32 |load1=9.850;10.000;12.000;0; load5=11.810;10.000;12.000;0; load15=9.320;10.000;12.000;0; %idle=52%;10;5;0 %user=34%;90;95;0 %system=3%;90;95;0 %iowait=11%;90;95;0 ram_free_percent=77%;10;5;0;100 ram_free=18524MB;2415;1207;0;24153 swap=3812MB;1528;764;0;3820 pswpout=0.0;250;500;0 pswpin=0.0;;;0 /=26171MB;120363;127050;0;133737 uptime=2598132s;1800;;0 ntp_offset=-0.000531s;5;10 
+OK - idle: 52% user: 34% system: 3% iowait: 11% interactive_mode: false</br>
+OK - 77% RAM free: 18524MB total: 24153MB</br>
+SWAP OK - 100% free (3812 MB out of 3820 MB) </br>
+OK - pswpout/s: 0.0 pswpin/s: 0.0 interactive_mode false</br>
+DISK OK - free space: / 100771 MB (79% inode=92%);</br>
+OK - Uptime 30 days 1 hours 42 minutes 12 seconds</br>
+OK - NTP offset: -0.000531 against 172.19.254.253</br>
+PROCS OK: 1 process with UID = 101 (ntp), command name 'ntpd'</br>
+PROCS OK: 1 process with UID = 0 (root), regex args '^/usr/sbin/cron$'</br>
+PROCS OK: 1 process with UID = 0 (root), regex args '^/usr/sbin/sshd$'</br>
+OK: No OOM killer activity found in dmesg output</br>
+$ echo $?
+1
+$ sudo /etc/init.d/ntp stop
+[ ok ] Stopping NTP server: ntpd.
+$ ruby check_linux.rb
+/usr/bin/ntpq: read: Connection refused
+WARNING - load average: 11.09, 11.98, 9.45 CRITICAL - NTP offset: 999 against unknown PROCS WARNING: 0 processes with UID = 101 (ntp), command name 'ntpd' |load1=11.090;10.000;12.000;0; load5=11.980;10.000;12.000;0; load15=9.450;10.000;12.000;0; %idle=52%;10;5;0 %user=34%;90;95;0 %system=3%;90;95;0 %iowait=11%;90;95;0 ram_free_percent=75%;10;5;0;100 ram_free=18210MB;2415;1207;0;24153 swap=3812MB;1528;764;0;3820 pswpout=0.0;250;500;0 pswpin=0.0;;;0 /=26171MB;120363;127050;0;133737 uptime=2598157s;1800;;0 ntp_offset=999s;5;10
+OK - idle: 52% user: 34% system: 3% iowait: 11% interactive_mode: false</br>
+OK - 75% RAM free: 18210MB total: 24153MB</br>
+SWAP OK - 100% free (3812 MB out of 3820 MB) </br>
+OK - pswpout/s: 0.0 pswpin/s: 0.0 interactive_mode false</br>
+DISK OK - free space: / 100771 MB (79% inode=92%);</br>
+OK - Uptime 30 days 1 hours 42 minutes 37 seconds</br>
+PROCS OK: 1 process with UID = 0 (root), regex args '^/usr/sbin/cron$'</br>
+PROCS OK: 1 process with UID = 0 (root), regex args '^/usr/sbin/sshd$'</br>
+OK: No OOM killer activity found in dmesg output</br>
+$ echo $?
+2
+$ ruby check_linux.rb
+OK - everything looks okay|load1=9.360;10.000;12.000;0; load5=8.270;10.000;12.000;0; load15=8.520;10.000;12.000;0; %idle=47%;10;5;0 %user=46%;90;95;0 %system=3%;90;95;0 %iowait=4%;90;95;0 ram_free_percent=79%;10;5;0;100 ram_free=19060MB;2415;1207;0;24153 swap=3812MB;1528;764;0;3820 pswpout=0.15;250;500;0 pswpin=0.0;;;0 /=26166MB;120363;127050;0;133737 uptime=2598689s;1800;;0 ntp_offset=-0.000523s;5;10
+OK - load average: 9.36, 8.27, 8.52</br>
+OK - idle: 47% user: 46% system: 3% iowait: 4% interactive_mode: false</br>
+OK - 79% RAM free: 19060MB total: 24153MB</br>
+SWAP OK - 100% free (3812 MB out of 3820 MB) </br>
+OK - pswpout/s: 0.15 pswpin/s: 0.0 interactive_mode false</br>
+DISK OK - free space: / 100777 MB (79% inode=92%);</br>
+OK - Uptime 30 days 1 hours 51 minutes 29 seconds</br>
+OK - NTP offset: -0.000523 against 172.19.255.253</br>
+PROCS OK: 1 process with UID = 101 (ntp), command name 'ntpd'</br>
+PROCS OK: 1 process with UID = 0 (root), regex args '^/usr/sbin/cron$'</br>
+PROCS OK: 1 process with UID = 0 (root), regex args '^/usr/sbin/sshd$'</br>
+OK: No OOM killer activity found in dmesg output</br>
+$ echo $?
+0
+```
---
+++ check_linux.rb
@@ -0,0 +1,505 @@
+#! /usr/bin/env ruby
+
+# -----------------------
+# Author: Andreas Paul (xorpaul) <xorpaul@gmail.com>
+# Date: 2013-12-02 10:57
+# Version: 0.1
+# -----------------------
+
+require 'optparse'
+require 'yaml'
+
+
+$debug = false
+$plugin_dir = '/usr/lib/nagios/plugins'
+if !File.exist?($plugin_dir)
+  $plugin_dir = '/usr/lib64/nagios/plugins'
+end
+# Pre-flight checks
+if !File.exist?("#{$plugin_dir}/check_load")
+  puts "ERROR: You need to install the nagios-plugins package!"
+  exit 3
+end
+if !File.exist?(`which sar`.chomp("\n"))
+  puts "ERROR: You need to install the sysstat package!"
+  exit 3
+end
+
+# The config file should be somwhere where only root and nagios can edit
+# Otherwise people could mess with your thresholds
+configfile = '/tmp/check_linux.cfg.yml'
+#configfile = '/var/lib/nagios/check_linux.cfg.yml'
+
+
+opt = OptionParser.new
+opt.on("--configfile [FILE]", "-c", "Config file to use in YAML format, defaults to #{configfile}") do |file|
+    configfile = file
+end
+opt.on("--debug", "-d", "print debug information, defaults to #{$debug}") do |f|
+    $debug = true
+end
+opt.parse!
+
+if File.exists?(configfile)
+  begin
+    config = YAML.load_file(configfile)
+  rescue
+    puts "UNKNOWN - failed to parse config file: #{configfile}"
+    exit 3
+  end
+  puts "Using config settings: #{config}" if $debug
+else
+  config = {}
+  # check_load defaults
+  config['check_load'] = {}
+  pc = `grep -c vendor_id /proc/cpuinfo`.to_i
+  config['check_load']['warn'] = "#{pc + 2},#{pc + 2},#{pc + 2}"
+  config['check_load']['crit'] = "#{pc + 4},#{pc + 4},#{pc + 4}"
+  # check_sar_cpu defaults
+  config['check_sar_cpu'] = {}
+  config['check_sar_cpu']['warn'] = 90
+  config['check_sar_cpu']['crit'] = 95
+  # check_sar_swap defaults
+  config['check_sar_swap'] = {}
+  config['check_sar_swap']['warn'] = 250
+  config['check_sar_swap']['crit'] = 500
+  # check_swap defaults
+  config['check_swap'] = {}
+  config['check_swap']['warn'] = '40%'
+  config['check_swap']['crit'] = '20%'
+  # check_disk defaults
+  config['check_disk'] = {}
+  config['check_disk']['/'] = {}
+  config['check_disk']['/']['warn'] = '10%'
+  config['check_disk']['/']['crit'] = '5%'
+  # check_mem defaults
+  config['check_mem'] = {}
+  config['check_mem']['warn'] = 10
+  config['check_mem']['crit'] = 5
+  # check_ntp defaults
+  config['check_ntp'] = {}
+  config['check_ntp']['warn'] = 5
+  config['check_ntp']['crit'] = 10
+  puts "Using default settings: #{config}" if $debug
+  File.open(configfile, 'w') do |f|
+    f.write(config.to_yaml)
+  end
+end
+
+def debug_header(function_name)
+  if $debug
+    puts "# ------------------------------"
+    puts "# %s" % function_name.to_s
+    puts "# ------------------------------"
+  end
+end
+
+# http://stackoverflow.com/a/4136485/682847
+def humanize(secs)
+  [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+    if secs > 0
+      secs, n = secs.divmod(count)
+      "#{n.to_i} #{name}"
+    end
+  }.compact.reverse.join(' ')
+end
+
+def check_load(warn, crit)
+  debug_header('check_load')
+  load_cmd = "#{$plugin_dir}/check_load -w #{warn} -c #{crit}"
+  puts "executing #{load_cmd}" if $debug
+  load_data = `#{load_cmd}`.split('|')
+  load_result = {}
+  load_result['returncode'] = $?.exitstatus
+  load_result['text'] = load_data[0]
+  load_result['perfdata'] = load_data[1].chomp().strip() if load_data.size >= 2
+  puts load_result if $debug
+  return load_result
+end
+
+def check_sar_cpu(warn, crit, interactive_mode=false)
+  debug_header('check_sar_cpu')
+  sar_cpu_result = {}
+  regex = /([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)$/
+  if interactive_mode == true
+    sar_cpu_cmd = "`which sar` 25 1"
+    puts "executing #{sar_cpu_cmd}" if $debug
+    sar_cpu_output = `#{sar_cpu_cmd}`
+    sar_cpu_data = sar_cpu_output.split("\n")
+    m = regex.match(sar_cpu_data[sar_cpu_data.size()-2])
+  else
+    sar_cpu_cmd = "`which sar`"
+    puts "executing #{sar_cpu_cmd}" if $debug
+    sar_cpu_output = `#{sar_cpu_cmd}`
+    sar_cpu_data = sar_cpu_output.split("\n")
+    m = regex.match(sar_cpu_data[sar_cpu_data.size()-2])
+    if m == nil
+      m = regex.match(sar_cpu_data[sar_cpu_data.size()-4])
+    end
+    if m == nil
+      interactive_mode = true
+      File.open('/tmp/check_linux_debug.txt', 'w+') { |file| file.write(sar_cpu_data) }
+      sar_cpu_cmd = "`which sar` 25 1"
+      puts "executing #{sar_cpu_cmd}, because regex did not match!" if $debug
+      sar_cpu_output = `#{sar_cpu_cmd}`
+      sar_cpu_data = sar_cpu_output.split("\n")
+      m = regex.match(sar_cpu_data[sar_cpu_data.size()-2])
+    end
+  end
+  if m != nil 
+    sar_cpu_user = m[1].to_f.round()
+    sar_cpu_system = m[3].to_f.round()
+    sar_cpu_iowait = m[4].to_f.round()
+    sar_cpu_idle = m[6].to_f.round()
+    if (100 - sar_cpu_idle) >= crit
+      text = "CRITICAL - idle: #{sar_cpu_idle}% < #{100 - crit}%"
+      rc = 2
+    elsif (100 - sar_cpu_idle) >= warn
+      text = "WARNING - idle: #{sar_cpu_idle}% < #{100 - warn}%"
+      rc = 1
+    else
+      text = "OK - idle: #{sar_cpu_idle}%"
+      rc = 0
+    end
+  sar_cpu_result['returncode'] = rc
+  sar_cpu_result['text'] = "#{text} user: #{sar_cpu_user}% system: #{sar_cpu_system}% iowait: #{sar_cpu_iowait}% interactive_mode: #{interactive_mode}"
+  sar_cpu_result['perfdata'] = "%idle=#{sar_cpu_idle}%;#{100-warn};#{100-crit};0 %user=#{sar_cpu_user}%;#{warn};#{crit};0 %system=#{sar_cpu_system}%;#{warn};#{crit};0 %iowait=#{sar_cpu_iowait}%;#{warn};#{crit};0"
+  else
+    puts "Regex did not match!" if $debug
+    sar_cpu_result['returncode'] = 1
+    sar_cpu_result['text'] = "WARNING: check_sar_cpu() regex did not match!"
+    sar_cpu_result['perfdata'] = "%idle=0%;#{100-warn};#{100-crit};0 %user=0%;#{warn};#{crit};0 %system=0%;#{warn};#{crit};0 %iowait=0%;#{warn};#{crit};0"
+    # XXX
+    File.open('/tmp/check_linux_debug.txt', 'w+') { |file| file.write(sar_cpu_data) }
+  end
+  puts sar_cpu_result if $debug
+  return sar_cpu_result
+end
+
+def check_sar_swap(warn, crit, interactive_mode)
+  debug_header('check_sar_swap')
+  sar_swap_result = {}
+  regex = /([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)$/
+  if interactive_mode == true
+    sar_swap_cmd = "`which sar` -W 25 1"
+    puts "executing #{sar_swap_cmd}" if $debug
+    sar_swap_output = `#{sar_swap_cmd}`
+    sar_swap_data = sar_swap_output.split("\n")
+    m = regex.match(sar_swap_data[sar_swap_data.size()-2])
+  else
+    sar_swap_cmd = "`which sar` -W"
+    puts "executing #{sar_swap_cmd}" if $debug
+    sar_swap_output = `#{sar_swap_cmd}`
+    sar_swap_data = sar_swap_output.split("\n")
+    m = regex.match(sar_swap_data[sar_swap_data.size()-2])
+    if m == nil
+      m = regex.match(sar_swap_data[sar_swap_data.size()-4])
+    end
+    if m == nil
+      interactive_mode = true
+      File.open('/tmp/check_linux_debug.txt', 'w+') { |file| file.write(sar_swap_data) }
+      sar_swap_cmd = "`which sar` -W 25 1"
+      puts "executing #{sar_swap_cmd}, because regex did not match!" if $debug
+      sar_swap_output = `#{sar_swap_cmd}`
+      sar_swap_data = sar_swap_output.split("\n")
+      m = regex.match(sar_swap_data[sar_swap_data.size()-2])
+    end
+  end
+  if m != nil
+    pswpout = m[1].to_f
+    pswpin = m[2].to_f
+    if pswpout >= crit
+      text = 'CRITICAL'
+      rc = 2
+    elsif pswpout >= warn
+      text = 'WARNING'
+      rc = 1
+    else
+      text = 'OK'
+      rc = 0
+    end
+    sar_swap_result['returncode'] = rc
+    sar_swap_result['text'] = "#{text} - pswpout/s: #{pswpout} pswpin/s: #{pswpin} interactive_mode #{interactive_mode}"
+    sar_swap_result['perfdata'] = "pswpout=#{pswpout};#{warn};#{crit};0 pswpin=#{pswpin};;;0"
+  else
+    puts "Regex did not match!" if $debug
+    sar_swap_result['returncode'] = 1
+    sar_swap_result['text'] = "WARNING: check_sar_swap() regex did not match!"
+    sar_swap_result['perfdata'] = "pswpout=0;#{warn};#{crit};0 pswpout=0;;;0"
+  end
+  puts sar_swap_result if $debug
+  return sar_swap_result
+end
+
+def check_swap(warn, crit)
+  debug_header('check_swap')
+  swap_cmd = "#{$plugin_dir}/check_swap -w #{warn} -c #{crit}"
+  puts "executing #{swap_cmd}" if $debug
+  swap_data = `#{swap_cmd}`.split('|')
+  swap_result = {}
+  swap_result['returncode'] = $?.exitstatus
+  swap_result['text'] = swap_data[0]
+  swap_result['perfdata'] = swap_data[1].chomp().strip()
+  puts swap_result if $debug
+  return swap_result
+end
+
+def check_disk(warn, crit, partition)
+  debug_header("check_disk partition #{partition}")
+  disk_cmd = "#{$plugin_dir}/check_disk -w #{warn} -c #{crit} #{partition}"
+  puts "executing #{disk_cmd}" if $debug
+  disk_data = `#{disk_cmd}`.split('|')
+  disk_result = {}
+  disk_result['returncode'] = $?.exitstatus
+  disk_result['text'] = disk_data[0]
+  disk_result['perfdata'] = disk_data[1].chomp().strip() if disk_data.size >= 2
+  puts disk_result if $debug
+  return disk_result
+end
+
+def check_proc(name, owner='root', check_with_regex=true)
+  debug_header("check_proc with #{name}")
+  absolute_path = `which #{name}`
+  # if /usr/sbin is not in your PATH then fall back to fuzzier -C check_proc
+  if check_with_regex and absolute_path != ''
+    proc_pattern = "--ereg-argument-array \"^#{absolute_path.chomp("\n")}$\""
+  else
+    proc_pattern = "-C #{name}"
+  end
+  proc_cmd = "#{$plugin_dir}/check_procs -w 1: -u #{owner} #{proc_pattern}"
+  puts "executing #{proc_cmd}" if $debug
+  proc_data = `#{proc_cmd}`
+  proc_result = {}
+  proc_result['returncode'] = $?.exitstatus
+  proc_result['text'] = proc_data.chomp()
+  proc_result['perfdata'] = ''
+  puts proc_result if $debug
+  return proc_result
+end
+
+def check_ram(warn, crit)
+  debug_header('check_ram')
+  mf = '/proc/meminfo'
+  puts "inspecting #{mf}" if $debug
+  total, free = 0, 0
+  File.open(mf, 'r').each do |line|
+    case line
+    when /^(?:MemTotal:)\s+([0-9]+) kB$/
+      total = $~[1].to_i
+    when /^(?:MemFree|Buffers|Cached|SReclaimable):\s+([0-9]+) kB$/
+      free += $~[1].to_i
+    end
+  end
+  ram_result = {}
+  # MB is better readable than kB
+  free /= 1024
+  total /= 1024
+  percent_free = (free / (total / 100.0)).round()
+  if percent_free <= crit
+    text = 'CRITICAL'
+    rc = 2
+  elsif percent_free <= warn
+    text = 'WARNING'
+    rc = 1
+  else
+    text = 'OK'
+    rc = 0
+  end
+  ram_result['returncode'] = rc
+  ram_result['text'] = "#{text} - #{percent_free}% RAM free: #{free}MB total: #{total}MB"
+  ram_result['perfdata'] = "ram_free_percent=#{percent_free}%;#{warn};#{crit};0;100 ram_free=#{free}MB;#{(warn * total) / 100};#{(crit * total) / 100};0;#{total}"
+  puts ram_result if $debug
+  return ram_result
+end
+
+def check_ntp(warn, crit)
+  debug_header('check_ntp')
+  ntp_cmd = "`which ntpq` -np"
+  puts "executing #{ntp_cmd}" if $debug
+  ntp_output = `#{ntp_cmd}`.split("\n")
+  ntp_offset, ntp_server = 999, 'unknown'
+  ntp_output.each do |line|
+    if line =~ /^\*/
+      ntp_data = line.split("\s")
+      # offset is in ms not in seconds!
+      ntp_offset = ntp_data[-2].to_f / 1000
+      ntp_server = ntp_data[0][1..-1]
+    end
+  end
+  ntp_result = {}
+  if ntp_offset >= crit
+    text = 'CRITICAL'
+    rc = 2
+  elsif ntp_offset >= warn
+    text = 'WARNING'
+    rc = 1
+  else
+    text = 'OK'
+    rc = 0
+  end
+  ntp_result['returncode'] = rc
+  ntp_result['text'] = "#{text} - NTP offset: #{ntp_offset} against #{ntp_server}"
+  ntp_result['perfdata'] = "ntp_offset=#{ntp_offset}s;#{warn};#{crit}"
+  puts ntp_result if $debug
+  return ntp_result
+end
+
+def check_uptime(secs)
+  debug_header('check_uptime')
+  upf = '/proc/uptime'
+  puts "inspecting #{upf}" if $debug
+  m = nil
+  File.open(upf, 'r').each do |line|
+    m = line.match(/^([0-9]+)\./)
+  end
+  uptime_result = {}
+  if m
+    sec_since_boot = m[1].to_i
+    if sec_since_boot <= secs
+      uptime_result['returncode'] = 1
+      uptime_result['text'] = "WARNING - Uptime #{humanize(sec_since_boot)}"
+      uptime_result['perfdata'] = "uptime=#{m[1]}s;#{secs};;0"
+    else
+      uptime_result['returncode'] = 0
+      uptime_result['text'] = "OK - Uptime #{humanize(sec_since_boot)}"
+      uptime_result['perfdata'] = "uptime=#{m[1]}s;#{secs};;0"
+    end
+  else
+    uptime_result['returncode'] = 3
+    uptime_result['text'] = 'UNKNOWN: Uptime regex failed to match'
+    uptime_result['perfdata'] = "uptime=0s;#{secs};;0"
+  end
+  puts uptime_result if $debug
+  return uptime_result
+end  
+
+def check_oom()
+  debug_header('check_oom')
+  oom_cmd = "dmesg | grep 'invoked oom-killer:'"
+  puts "executing #{oom_cmd}" if $debug
+  oom_data = `#{oom_cmd}`
+  oom_result = {}
+  if oom_data != ''
+    oom_result['returncode'] = 1
+    oom_result['text'] = "WARNING: OOM killer was invoked! Check dmesg output and clear it with dmesg -c when finished: #{oom_data.split("\n")[0]}</br>"
+    oom_result['perfdata'] = ''
+  else
+    oom_result['returncode'] = 0
+    oom_result['text'] = "OK: No OOM killer activity found in dmesg output"
+    oom_result['perfdata'] = ''
+  end
+  puts oom_result if $debug
+  return oom_result
+end
+
+def check_os()
+  debug_header('check_os')
+  type = 'Debian'
+  if File.exists?('/etc/redhat-release')
+    type = 'RedHat'
+  elsif File.exists?('/etc/lsb-release')
+    type = 'Ubuntu'
+  end
+  return type
+end
+
+$os_type = check_os()
+
+# Actually call the check functions
+
+results = []
+results << check_load(config['check_load']['warn'], config['check_load']['crit'])
+
+# Use sar in interactive mode if the sysstat data file can not be found
+sar_interactive_mode = false
+if File.exists?("/var/log/sysstat/sa#{Time.now.strftime("%d")}") == false
+  sar_interactive_mode = true
+  sar_cpu_thread = Thread.new{results << check_sar_cpu(config['check_sar_cpu']['warn'], config['check_sar_cpu']['crit'], sar_interactive_mode)}
+else
+  results << check_sar_cpu(config['check_sar_cpu']['warn'], config['check_sar_cpu']['crit'], sar_interactive_mode)
+end
+results << check_ram(config['check_mem']['warn'], config['check_mem']['crit'])
+
+# Only check for swap size and activity if there is any swap present
+has_swap = false
+File.open('/proc/meminfo', 'r').each do |line|
+  case line
+  when /^(?:SwapTotal:)\s+([0-9]+) kB$/
+    has_swap = true if $~[1].to_i > 0
+  end
+end
+if has_swap
+  results << check_swap(config['check_swap']['warn'], config['check_swap']['crit'])
+  if sar_interactive_mode
+    sar_swap_thread = Thread.new{results << check_sar_swap(config['check_sar_swap']['warn'], config['check_sar_swap']['crit'], sar_interactive_mode)}
+  else
+    results << check_sar_swap(config['check_sar_swap']['warn'], config['check_sar_swap']['crit'], sar_interactive_mode)
+  end
+end
+
+config['check_disk'].each do |partition, thresholds|
+  results << check_disk(thresholds['warn'], thresholds['crit'], partition)
+end
+results << check_uptime(1800)
+results << check_ntp(config['check_ntp']['warn'], config['check_ntp']['crit'])
+results << check_proc('ntpd', 'ntp', false)
+case $os_type
+  when 'Debian'
+    # Debian cron process name is /usr/sbin/cron
+    results << check_proc('cron')
+    results << check_proc('sshd')
+  when 'RedHat'
+    # and on RedHat it's crond
+    results << check_proc('crond', 'root', false)
+    results << check_proc('sshd')
+  when 'Ubuntu'
+    # But on Ubuntu it's just cron
+    results << check_proc('cron', 'root', false)
+    # Ubuntu uses sshd -D, so we can't match with regex sshd$
+    results << check_proc('sshd', 'root', false)
+end
+#results << check_proc('rsyslogd', 'root', false)
+results << check_oom()
+
+# Wait for the interactive sar threads to terminate
+# Appending to an array is thread safe/atomic operation
+# http://stackoverflow.com/a/17767589/682847
+if sar_interactive_mode
+  sar_cpu_thread.join
+  if has_swap
+    sar_swap_thread.join
+  end
+end
+puts "\n\nresult array: #{results}\n\n" if $debug
+
+
+# Aggregate check results
+
+output = {}
+output['returncode'] = 0
+output['text'] = ''
+output['multiline'] = ''
+output['perfdata'] = ''
+results.each do |result|
+  output['perfdata'] += "#{result['perfdata']} " if result['perfdata'] != ''
+  if result['returncode'] >= 1
+    output['text'] += "#{result['text']} "
+    case result['returncode']
+    when 3
+      output['returncode'] = 3 if result['returncode'] > output['returncode']
+    when 2
+      output['returncode'] = 2 if result['returncode'] > output['returncode']
+    when 1
+      output['returncode'] = 1 if result['returncode'] > output['returncode']
+    end
+  else
+    output['multiline'] += "#{result['text']}</br>\n"
+  end
+end
+if output['text'] == ''
+  output['text'] = 'OK - everything looks okay'
+end
+
+puts "#{output['text']}|#{output['perfdata']}\n#{output['multiline'].chomp()}"
+
+exit output['returncode']
